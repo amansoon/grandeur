37e30327aedfdc2c00402f275f46b135a514ee0b
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-03-18 20:30:36 +0530

    initial commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import css from './styles/sidebar.module.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ pages/product.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// eslint-disable-next-line
+
+function Product() {
+  return (
+    <div>product</div>
+  )
+}
+
+export default product
